9ee98040bd396d5bb45029c8ba73f78ad5621af7
VKm18 <rahul.vinodhkumar@students.mq.edu.au>
2023-10-13 20:11:51 +1100

    changing bank.csv into an RData file

---
+++ R/data_bank.R
@@ -0,0 +1,12 @@
+#' Bank Queue Data
+#'
+#' A dataset containing example bank queue data.
+#'
+#' @format A data frame with 100 rows and 2 columns for arrival time and service time.
+#'
+#' @source Mention where you got the data, if applicable.
+#'
+#' @docType data
+#' @name bank
+#' @usage data(bank)
+NULL
